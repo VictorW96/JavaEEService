575650dde27cb761bb360f6b7cc3c3abdae69570
Victor Wolf <victor.wolf@qaware.de>
2021-07-29 13:00:56 +0200

    gradlew build

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,11 +1,16 @@
 pipeline {
 
-    agent { docker { image 'maven:3.3.3' } }
+    agent { docker { image 'azul/zulu-openjdk:8u181' } }
 
     stages {
         stage('Checkout') {
             steps { //Checking out the repo
                 checkout([$class: 'GitSCM', branches: [[name: '*/master']], extensions: [], userRemoteConfigs: [[credentialsId: '996990e9-64f3-4954-ad77-e08ff1b20f4d', url: 'https://github.com/VictorW96/JavaEEService']]])
+            }
+        }
+        stage('Build') {
+            steps { //Checking out the repo
+                sh './gradlew build'
             }
         }
     }
